e008bb24ea5c6281fafd9a193dcfcd89a5abaed9
Winnie Song <winniesong0121@gmail.com>
2019-07-26 13:27:36 -0400

    api and get tests paased. React app created

---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home(props) {
+  return (
+    <section className="home-container">
+      <h1>Welcome to Dad Jokes!</h1>
+    </section>
+  );
+}
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as DadJokes } from './DadJokes';
+export { default as NavBar } from './NavBar';
